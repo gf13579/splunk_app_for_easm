9980df6c0e3562a70222c87903e85276865b39ea
gf13579 <greg.ford.2@gmail.com>
2024-09-12 15:37:13 +1000

    Updating splunklib

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ if [ ! -f package.sh ]; then
     exit 1
 fi
 
-APP_DIR="splunk_app_for_easm"
+APP_DIR=${PWD##*/}
 
 echo "Creating ${APP_DIR}.tgz"
 
@@ -14,8 +14,9 @@ find . -type d -name __pycache__ -delete
 COPYFILE_DISABLE=1 tar czf "${APP_DIR}.tgz" -C ../ \
     --exclude '*/__pycache__/*' \
     --exclude '.git*' \
+    --exclude '.vscode*' \
     --exclude '*.tgz' \
     --exclude '*.sh' \
+    --exclude 'local' \
+    --exclude 'metadata/local.meta' \
     "${APP_DIR}"
-
-echo "Done!"
